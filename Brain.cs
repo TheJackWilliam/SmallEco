6a63a980e0230a59d923d7d7fe9bc10f69104333
TheJackWilliam <smartcap25@gmail.com>
2023-10-25 11:46:14 -0400

    Added some private variables as a jumping-off point.

--- Brain.cs
+++ Brain.cs
@@ -1,11 +1,22 @@
+using System;
 using Godot;
 
 public partial class Brain : Node2D
 {
-	private int[, ] jkfsdaklfjdsa;
+	private int[][] _networkWeights;
+	private int[] _layerSizes;
+	private const int BiasOffset = 1;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Random rand = new Random();
+		for (int layer = 0; layer < _networkWeights.Length; layer++)
+		{
+			for (int weight = 0; weight < _networkWeights[layer].Length + BiasOffset; weight++)
+			{
+				weight = rand.Next();
+			}
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
